c631eccc4dc7e034577b76bff4d869badbb8667c
Nick Snyder <n.a.snyder@comcast.net>
2017-08-22 18:58:57 -0400

    Populated board array.  Created turns.

--- js/main.js
+++ js/main.js
@@ -1,25 +1,51 @@
 $(document).ready(function(){
-  var boardArr = [];
+  var boardArr = [[],[],[],[],[],[],[]];
 
   var boardhtml = "";
 
-  var piece;
+  var piece = "url(img/red.png)";
 
-  var turn;
+  var turn = "red";
 
+
+  //Create board
   for(var i = 0; i < 7; i++){
-    boardhtml += "<div class='aboveBoardSection btn'></div>"
-  }
+    boardhtml += "<div id='box"+i+"' class='aboveBoardSection'></div>";
+  };
   for(var i = 7; i < 49; i++){
-    boardhtml += "<img class='img-responsive' src='img/ConnectFour.png'>";
-  }
-
+    boardhtml += "<img id='box"+i+"' class='img-responsive' src='img/ConnectFour.png'>";
+  };
   $(".board").html(boardhtml);
 
-  $(".aboveBoardSection").hover(function(){
+  //Populate board Array
+  for(var r = 0; r < 7; r++){
+    for(var c = 0; c < 7; c++){
+      boardArr[r].push($("#box" + (c+(7*r))));
+    };
+  };
+  console.log(boardArr);
 
-    $(this).css("background-image", "url(img/red.png)");
+  //Show piece above board
+  $(".aboveBoardSection").hover(function(){
+    $(this).css("background-image", piece);
   }, function(){
     $(this).css("background-image", "none");
+  });
+
+  //Drop a piece, make a turn
+  $(".aboveBoardSection").click(function(){
+    
+
+
+    if(turn == "red"){
+      piece = "url(img/black.png)";
+      turn = "black";
+      return;
+    };
+    if(turn == "black"){
+      piece = "url(img/red.png)";
+      turn = "red";
+      return;
+    };
   });
 });
